95dc702938c4fc7872712792cd831c692758676a
rcd-systems <comptepoubelle8@gmail.com>
2017-01-24 20:40:01 +0100

    Add parameters capability to router state

--- src/main/resources/assets/js/rcd-jsfwk-router.js
+++ src/main/resources/assets/js/rcd-jsfwk-router.js
@@ -14,15 +14,17 @@ class RcdHistoryRouter {
         return this;
     }
 
-    addRoute(state, callback) {
-        this.routes[state] = callback;
+    addRoute(route, callback) {
+        this.routes[route] = callback;
         return this;
     }
 
     setState(state) {
         if (state) {
             history.pushState(state, null, '#' + state);
-            this.routes[state]();
+            var parametersIndex = state.indexOf("?");
+            var route = parametersIndex == -1 ? state : state.substring(0, parametersIndex);
+            this.routes[route]();
         } else {
             this.defaultRoute();
             history.pushState(state, null, '#');
@@ -33,5 +35,20 @@ class RcdHistoryRouter {
 
     getCurrentState() {
         return location.hash && location.hash.substring(1);
+    }
+
+    getParameters() {
+        var currentState = this.getCurrentState();
+        var parametersIndex = currentState.indexOf("?");
+
+        var parameters = {};
+        if (parametersIndex !== -1) {
+            var parametersString = currentState.substring(parametersIndex + 1);
+            parametersString.split('&').forEach((parameterEntry) => {
+                var parameterEntryValue = parameterEntry.split('=');
+                parameters[parameterEntryValue[0]] = parameterEntryValue[1];
+            });
+        }
+        return parameters;
     }
 }
