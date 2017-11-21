433a83e0469ba7eb1dbe65ebd6686a0827ba0323
rcd-systems <comptepoubelle8@gmail.com>
2017-11-22 00:29:42 +0100

    toLocalDateFormat and toLocalTimeFormat functions

--- src/main/resources/assets/js/rcd-jsfwk-time.js
+++ src/main/resources/assets/js/rcd-jsfwk-time.js
@@ -1,8 +1,16 @@
-function toLocalDateTimeFormat(date, dateSeparator = '-', timeSeparator = ':') {
+function toLocalDateTimeFormat(date = new Date(), dateSeparator = '-', timeSeparator = ':') {
+    return "" + toLocalDateFormat(date, dateSeparator) + 'T' +
+           toLocalTimeFormat(date, timeSeparator);
+}
+
+function toLocalDateFormat(date = new Date(), dateSeparator = '-') {
     return "" + date.getFullYear() + dateSeparator +
            toDoubleDigitFormat(date.getMonth() + 1) + dateSeparator +
-           toDoubleDigitFormat(date.getDate()) + 'T' +
-           toDoubleDigitFormat(date.getHours()) + timeSeparator +
+           toDoubleDigitFormat(date.getDate());
+}
+
+function toLocalTimeFormat(date = new Date(), timeSeparator = ':') {
+    return "" + toDoubleDigitFormat(date.getHours()) + timeSeparator +
            toDoubleDigitFormat(date.getMinutes()) + timeSeparator +
            toDoubleDigitFormat(date.getSeconds());
 }
