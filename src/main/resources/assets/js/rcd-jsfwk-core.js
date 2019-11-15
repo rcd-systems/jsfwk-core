9e04c0a6fc37b038fe28a8cd629ad5c835492a7f
rcd-systems <comptepoubelle8@gmail.com>
2019-11-15 23:00:19 +0100

    Use include-maven-plugin for JS merge

---
+++ src/main/resources/assets/js/rcd-jsfwk-core.js
@@ -0,0 +1,7 @@
+include('rcd-jsfwk-base.js');
+include('rcd-jsfwk-xml.js');
+include('rcd-jsfwk-html.js');
+include('rcd-jsfwk-html-elements.js');
+include('rcd-jsfwk-html-text.js');
+include('rcd-jsfwk-router.js');
+include('rcd-jsfwk-time.js');
